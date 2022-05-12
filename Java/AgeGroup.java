f712779a04f0cf29fe864f2fc4f6294e80482677
KTonev <42643264+KTonev@users.noreply.github.com>
2022-05-12 14:08:17 +0300

    Delete Java directory

--- Java/AgeGroup.java
+++
@@ -1,5 +0,0 @@
-package com.CITB408.Kontrolno1;
-
-public enum AgeGroup {
-    ADULTS, TEENAGERS, CHILDREN
-}
--- Java/IsPaidByNHIF.java
+++
@@ -1,5 +0,0 @@
-package com.CITB408.Kontrolno1;
-
-public enum IsPaidByNHIF {
-    PAID_BY_NHIF, NOT_PAID_BY_NHIF
-}
